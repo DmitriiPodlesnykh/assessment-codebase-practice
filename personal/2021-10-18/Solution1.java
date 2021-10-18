73474c91f817b459b6964a0473ed315119f33340
Dmitrii Podlesnykh <podlesnykh.dmitrii@gmail.com>
2021-10-19 01:22:53 +0300

    Update Solution1.java

--- personal/2021-10-18/Solution1.java
+++ personal/2021-10-18/Solution1.java
@@ -3,30 +3,72 @@ import java.util.*;
 import static java.util.stream.Collectors.*;
 
 
-public class Solution1 {
+public class Solution {
     
     public static void main(String[] args) {
         final List<String> result = DataProvider.prepareInputParams().stream()
         .map(e -> update(e))
         .collect(toList());
-        System.out.println("myString is: " + result);
+        
+        result.forEach(e -> System.out.println(e));
     }
     
-    private static String update(InputRecord rec) {//Character.isWhitespace()
+    private static String update(InputRecord rec) {
         String result = "";
+        final String recValue = rec.value;
         if (rec.firstParam == FirstParam.S) {
-            result = toSeparateWords(rec);
+            result = split(recValue);
         } else if (rec.firstParam == FirstParam.C) {
-            
+            result = combine(recValue);
+        } else {
+            result = recValue;
+        }
+        
+        if (rec.secondParam == SecondParam.M) {
+            result = toMethod(result);
+        } else if (rec.secondParam == SecondParam.C) {
+            result = toClass(result);
         }
         return result;
     }
     
-    private static String toSeparateWords(InputRecord rec) {
+    private static String combine(String value) {
         String result = "";
-        String recValue = rec.value;
-        for (int i = rec.value.length()-1; i>=0; i--) {
-                char c = rec.value.charAt(i);
+        for (int i = 0; i<=value.length()-1; i++) {
+                char c = value.charAt(i);
+                if (Character.isWhitespace(c) ) {
+                    i++;
+                    result = result + Character.toUpperCase(value.charAt(i));
+                } else {
+                    result = result + c;
+                }
+        }
+        return result;
+    }
+    
+    private static String toClass(String value) {
+        String result = value;
+        if (Character.isWhitespace(result.charAt(0)) ) {
+            return result.substring(1);
+        } else if (Character.isLowerCase(result.charAt(0))) {
+            return Character.toUpperCase(result.charAt(0)) + result.substring(1);
+        } else {
+            return result;
+        }
+    }
+    
+    private static String toMethod(String value) {
+        if (value.contains("()")) {
+            return value;
+        } else {
+            return value + "()";
+        }
+    }
+    
+    private static String split(String value) {
+        String result = "";
+        for (int i = value.length()-1; i>=0; i--) {
+                char c = value.charAt(i);
                 if (Character.isUpperCase(c) ) {
                     result = result + Character.toLowerCase(c) + " ";
                 } else {
