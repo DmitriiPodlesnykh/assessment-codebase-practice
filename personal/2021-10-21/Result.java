590e295f7840884811ec4ec0bfbd1a8382c9233f
Dmitrii Podlesnykh <podlesnykh.dmitrii@gmail.com>
2021-10-21 13:41:26 +0300

    Create Result.java

---
+++ personal/2021-10-21/Result.java
@@ -0,0 +1,20 @@
+class Result {
+
+    /*
+     * Complete the 'lonelyinteger' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int lonelyinteger(List<Integer> a) {
+        return a.stream()
+                .collect(groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream()
+                .filter(e -> e.getValue() == 1)
+                .map(Map.Entry::getKey)
+                .findFirst()
+                .orElse(0);
+    }
+
+}
