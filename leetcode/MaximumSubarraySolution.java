e11a3121fce141a11401c90d5c574eb2d1601d3e
Dmitrii Podlesnykh <dmitrii.podlesnykh@gmail.com>
2021-12-11 22:09:12 +0300

    Update MaximumSubarraySolution.java

--- leetcode/MaximumSubarraySolution.java
+++ leetcode/MaximumSubarraySolution.java
@@ -1,8 +1,33 @@
 /**
  * Source: https://leetcode.com/problems/maximum-subarray
  *
+ * Time Submitted   | Status | Runtime| Memory
+ * 12/11/2021 22:02	|Accepted|16 ms   |	49 MB
+ * Runtime: 15 ms, faster than 6.23% of submissions. Memory Usage: 48.9 MB, less than 91.96% of submissions.
  */
 public class MaximumSubarraySolution {
+   public int maxSubArray(int[] nums) {
+        
+        int maxSumValue = nums[0];
+        for (int i = 0; i< nums.length; i++) {
+            
+            int sum = 0;
+            for (int j = i; j < nums.length; j++) {
+                int startValue = nums[j];
+                sum = sum + startValue;
+                
+                if (startValue > sum) {
+                    break;
+                } else if (maxSumValue < sum) {
+                    maxSumValue = sum;
+                }
+                if (j == nums.length-1 ) {
+                    return maxSumValue;
+                }
+            }
+        }
+        return maxSumValue;
+    }
 }
 
 /**
